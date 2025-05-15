9a2b36d43cb3e0bcc64d57f472e3551a3f58045b
hta393939 <hta393939@gmail.com>
2025-05-15 19:09:50 +0900

    bump

--- src/vrm-file-loader.ts
+++ src/vrm-file-loader.ts
@@ -1,17 +1,25 @@
-import { SceneLoader } from '@babylonjs/core/Loading/sceneLoader';
+//import { SceneLoader } from '@babylonjs/core/Loading/sceneLoader';
 import { GLTFFileLoader } from '@babylonjs/loaders/glTF/glTFFileLoader';
 import { RegisterSceneLoaderPlugin } from '@babylonjs/core/Loading/sceneLoader';
+import type { ISceneLoaderPluginExtensions } from '@babylonjs/core/Loading/sceneLoader';
 
 /**
  * VRM/VCI ファイルを読み込めるようにする
  * 拡張子を変更しただけ
  */
 export class VRMFileLoader extends GLTFFileLoader {
-    public readonly name = 'vrm';
-    public readonly extensions = {
-        '.vrm': { isBinary: true, mimeType: 'model/gltf-binary' },
-        '.vci': { isBinary: true, mimeType: 'model/gltf-binary' },
-    };
+    //name: string;
+    //extensions: string | ISceneLoaderPluginExtensions;
+
+    constructor() {
+        super();
+
+        (this.name as string) = 'vrm';
+        (this.extensions as ISceneLoaderPluginExtensions) = {
+            '.vrm': { isBinary: true, mimeType: 'model/gltf-binary' },
+            '.vci': { isBinary: true, mimeType: 'model/gltf-binary' },
+        };
+    }
 
     public createPlugin() {
         return new VRMFileLoader();
